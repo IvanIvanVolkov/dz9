c0afbae4dd9cc8fd858eea82fa62bdba4ffacf83
Ivan <off_2020@mail.ru>
2023-03-12 10:00:15 +0500

    t1

---
+++ t1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+int = = Convert.ToInt32(Console.ReadLine());
+1  = = 1;
+NaturalToLow(number, count);
+
+
+void NaturalToLow(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(n, count  + 1);
+        Console.Write (count  + " ");
+    }
+}
